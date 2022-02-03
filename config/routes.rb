645059dd7b4a8217cbafffa6dc4e67a1c9d8ff6d
shanebrown013 <shanebrown013@gmail.com>
2022-02-03 15:24:34 -0600

    Final Commit - Added delete page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :books
   root 'books#index'
+  get 'books/delete/:id', to:'books#delete', as:'delete_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
