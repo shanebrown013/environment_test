00a41e94cbbbbc792b8b1e215abee4ab0895a87a
shanebrown013 <shanebrown013@gmail.com>
2022-02-10 21:13:26 -0600

    Added Users and User_books

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  resources :user_books
+  resources :users
   resources :books
-  root 'books#index'
+  root 'user_books#index'
   get 'books/delete/:id', to:'books#delete', as:'delete_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :books do
+    member do
+      get :delete
+    end
+  end
+
 end
